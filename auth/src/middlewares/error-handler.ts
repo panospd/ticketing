7689a8d6776b0f8305616bd3f2d42fa0b079114b
Panos Anastasiadis <panos.anastasiadis@isometrix.com>
2022-07-04 21:46:57 +0100

    added custom errors and basic error structure

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,13 +1,30 @@
 import { Request, Response, NextFunction } from "express"
+import { DatabaseConnectionError } from "../errors/database-connection-error"
+import { RequestValidationError } from "../errors/request-validation-error"
 
 export const errorHandler = (
     err: Error, 
     req: Request, 
     res: Response, 
     next: NextFunction) => {
-    console.log("Something went wrong", err)
 
-    res.status(400).send({
-        message: err.message
-    })
+    if(err instanceof RequestValidationError) {
+        const formattedErrors = err.errors.map(error => {
+            return { message: error.msg, field: error.param }
+        })
+
+        return res.status(400).send({ errors: formattedErrors })
+    }
+    
+    if(err instanceof DatabaseConnectionError) {
+        return res.status(500).send({ errors: [
+                { message: err.reason }
+            ]
+        })
+    } 
+
+    return res.status(500).send({ errors: [
+        { message: "Something went wrong" }
+    ]
+})
 }
