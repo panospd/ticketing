40fe6e985606d1dc24756272e12cb2172ce4838b
Panos Anastasiadis <anastapd@gmail.com>
2022-07-16 09:35:43 +0100

    added validation logic for signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,32 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from "../errors/request-validation-error"
+import { User } from '../models/user'
 
 const router = express.Router()
 
-router.post("/api/users/signin", (req, res) => {
-    res.send("This is signin!")
-})
+router.post(
+    "/api/users/signin", 
+    [
+        body("email")
+            .isEmail()
+            .withMessage("Email must be valid"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("You must supply a password")
+    ], 
+    async (req: Request, res: Response) => {
+        var errors = validationResult(req);
+        
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array())
+        }
+
+        var user = await User.findOne({ email: req.body.email })
+
+        res.send("This is signin!")
+    }
+)
 
 export { router as signinRouter }
