647e74b461e2c8f4883eecedf48d6d1aff31612d
panospd <anastapd@gmail.com>
2022-07-22 10:17:12 +0100

    created app file to export express app

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,29 +1,5 @@
-import express from 'express';
-import "express-async-errors"
-import { json } from "body-parser"
 import mongoose, { mongo } from "mongoose"  
-
-import { currentUserRouter } from "./routes/current-user"
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import cookieSession from 'cookie-session';
-const app = express();
-app.set("trust proxy", true)
-
-app.use(json());
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}))
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.use(errorHandler)
+import { app } from "./app"
 
 const start =async () => {
     if (!process.env.JWT_KEY) {
@@ -41,5 +17,4 @@ app.listen(3000, () => {
     console.log("Listening on port 3000!");
 })
 
-start()
-
+start();
