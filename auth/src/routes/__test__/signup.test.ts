106046201eaa0e428750f88e1608666b75a5b223
panospd <anastapd@gmail.com>
2023-03-31 04:54:05 +0100

    added tests for signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,69 @@ it("returns a 201 on successful signup", async () => {
         })
         .expect(201)
 })
+
+it("returns a 400 with an invalid email", async () => {
+    return request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "testtest.com",
+            password: "password"
+        })
+        .expect(400)
+})
+
+it("returns a 400 with an invalid password", async () => {
+    return request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "test@test.com",
+            password: "p"
+        })
+        .expect(400)
+})
+
+it("returns a 400 with missing email and password", async () => {
+    await request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "test@test.com",
+        })
+        .expect(400)
+
+    return request(app)
+        .post("/api/users/signup")
+        .send({
+            password: "fdafdadap"
+        })
+        .expect(400)
+})
+
+it("disallows duplicate emails", async () => {
+    await request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "test@test.com",
+            password: "password"
+        })
+        .expect(201)
+
+    return await request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "test@test.com",
+            password: "password"
+        })
+        .expect(400)
+})
+
+it("sets a cookie after successful signup", async () => {
+    const res = await request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "test@test.com",
+            password: "password"
+        })
+        .expect(201)
+
+    expect(res.get("Set-Cookie")).toBeDefined()
+})
