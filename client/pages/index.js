d6d1c77a3c34e9d695ab202535f097f7b87a6609
panospd <anastapd@gmail.com>
2023-04-02 17:22:03 +0100

    implemented build client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,25 +1,14 @@
-import axios, { Axios } from "axios";
-
 import React from "react";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
     return <h1>{currentUser?.email}</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-    if (typeof window === "undefined") {
-        const res = await axios.get(
-            "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current",
-            {
-                headers: req.headers,
-            }
-        );
-
-        return res.data;
-    } else {
-        const { data } = await axios.get("/api/users/current");
-        return data;
-    }
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get("/api/users/current");
+    return data;
 };
 
 export default LandingPage;
