b89e0a7cb6ee983eaa242037cfa59d04ed8128a5
panospd <anastapd@gmail.com>
2023-04-02 11:33:36 +0100

    implemented sign up suing auth service

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,12 +1,25 @@
 import { useState } from "react";
+import axios from "axios";
 
 const signup = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
         console.log(email, password);
+
+        try {
+            const response = await axios.post("/api/users/signup", {
+                email,
+                password,
+            });
+
+            console.log(response.data);
+        } catch (error) {
+            setErrors(error.response.data.errors);
+        }
     };
 
     return (
@@ -29,7 +42,17 @@ const signup = () => {
                     className="form-control"
                 />
             </div>
-            <button className="btn btn-primary">Sign up</button>
+            {!!errors.length && (
+                <div className="alert alert-danger">
+                    <h4>Ooops...</h4>
+                    <ul className="my-0">
+                        {errors.map((err) => (
+                            <li key={err.message}>{err.message}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            <button className="btn btn-primary mt-2">Sign up</button>
         </form>
     );
 };
