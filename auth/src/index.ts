ba3f5b285e7541f35417616f78a846629e9299b7
panospd <anastapd@gmail.com>
2023-04-04 17:26:55 +0100

    added env variables for mongo uri for tickets and auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,7 +7,11 @@ const start = async () => {
         if (!process.env.JWT_KEY) {
             throw new Error('JWT_KEY must be defined')
         }
-        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth")
+
+        if (!process.env.MONGO_URI) {
+            throw new Error('MONGO_URI must be defined')
+        }
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDb...")
         app.listen(3000, () => {
             console.log("Listening on port 3000!!!!!!")
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,7 +7,12 @@ const start = async () => {
         if (!process.env.JWT_KEY) {
             throw new Error('JWT_KEY must be defined')
         }
-        await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets")
+
+        if (!process.env.MONGO_URI) {
+            throw new Error('MONGO_URI must be defined')
+        }
+
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDb...")
         app.listen(3000, () => {
             console.log("Listening on port 3000!!!!!!")
