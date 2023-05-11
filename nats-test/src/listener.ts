59e3a383cccb23b95f2ddb5826e7e6095aeed95f
panospd <anastapd@gmail.com>
2023-05-11 18:17:19 +0100

    added queue group and manual ack

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,22 @@
 import nats, { Message } from "node-nats-streaming"
-
+import { randomBytes } from "crypto"
 console.clear()
 
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
     url: "http://localhost:4222"
 })
 
 stan.on("connect", () => {
     console.log("Listener connected to NATS")
 
-    const subscription = stan.subscribe("ticket:created")
+    const options = stan
+        .subscriptionOptions()
+        .setManualAckMode(true);
+
+    const subscription = stan.subscribe(
+        "ticket:created",
+        "orders-service-queue-group",
+        options)
 
     subscription.on("message", (msg: Message) => {
         const data = msg.getData();
@@ -17,5 +24,7 @@ stan.on("connect", () => {
         if (typeof data === "string") {
             console.log(`Received event ${msg.getSequence()}, with data: ${data}`)
         }
+
+        msg.ack()
     })
 })
