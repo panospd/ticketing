46c1811f38e1357502117c61e67306b8ddd48171
panospd <anastapd@gmail.com>
2023-05-31 07:53:04 +0100

    implemented base publisher class

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,22 +1,20 @@
 import nats from "node-nats-streaming"
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher"
 
 const stan = nats.connect("ticketing", "abc", {
     url: "http://localhost:4222"
 })
 
-
 console.clear()
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
     console.log("Publisher connected to NATS")
 
-    const data = JSON.stringify({
+    const data = {
         id: "123",
         title: "concert",
         price: 20
-    })
+    }
 
-    stan.publish("ticket:created", data, () => {
-        console.log("Event published")
-    })
+    await new TicketCreatedPublisher(stan).publish(data);
 })
