327287ce58bec4333638f0af942236c98a559520
panospd <anastapd@gmail.com>
2023-06-03 16:17:17 +0100

    added test setup

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,52 @@
+import { MongoMemoryServer } from "mongodb-memory-server"
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+
+declare global {
+    var signin: (email?: string) => string[];
+}
+
+global.signin = (email?: string) => {
+    if (!email) {
+        email = "test@test.com"
+    }
+
+    const token = jwt.sign({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        email,
+    }, process.env.JWT_KEY!)
+
+    const sessionJson = JSON.stringify({ jwt: token })
+    const base64 = Buffer.from(sessionJson).toString("base64")
+
+    return [`session=${base64}`];
+}
+
+jest.mock("../nats-wrapper")
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+    process.env.JWT_KEY = "asdf"
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+
+    await mongoose.connect(mongoUri, {})
+})
+
+beforeEach(async () => {
+    jest.clearAllMocks();
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+        await collection.deleteMany({})
+    }
+})
+
+afterAll(async () => {
+    if (mongo) {
+        await mongo.stop();
+    }
+
+    await mongoose.connection.close()
+})
