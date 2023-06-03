d713046cbd994861d29f71c9dbcf5ed70257721a
panospd <anastapd@gmail.com>
2023-06-03 16:14:17 +0100

    implemented new order route handler

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,13 @@
-import { requireAuth, validateRequest } from "@ticketingpd/common";
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@ticketingpd/common";
 import express, { Request, Response } from "express"
 import { body } from "express-validator";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post("/api/orders", requireAuth, [
     body("ticketId")
@@ -12,8 +16,31 @@ router.post("/api/orders", requireAuth, [
         .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
         .withMessage("TicketId must be provided")
 ], validateRequest, async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
 
-    res.send({})
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+        throw new NotFoundError()
+    }
+
+    if (await ticket.isReserved()) {
+        throw new BadRequestError("Ticket is already reserved.")
+    }
+
+    const expiration = new Date()
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+
+    const order = Order.build({
+        userId: req.currentUser!.id,
+        expiresAt: expiration,
+        status: OrderStatus.Created,
+        ticket
+    })
+
+    await order.save();
+
+    res.status(201).send(order)
 })
 
 export { router as newOrderRouter }
