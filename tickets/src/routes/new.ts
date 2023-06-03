cbb618c973443e409afb53f822713a73461fe3d4
panospd <anastapd@gmail.com>
2023-06-03 05:01:55 +0100

    implemented sending ticket created

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from "express"
 import { body } from "express-validator"
 import { Ticket } from "../models/ticket"
 import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher"
+import { natsWrapper } from "../../nats-wrapper"
 
 const router = express.Router()
 
@@ -31,12 +32,12 @@ router.post(
 
         await ticket.save();
 
-        // await new TicketCreatedPublisher(client).publish({
-        //     id: ticket.id,
-        //     title: ticket.title,
-        //     price: ticket.price,
-        //     userId: ticket.userId
-        // })
+        await new TicketCreatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId
+        })
 
         return res.status(201).send(ticket)
     }
