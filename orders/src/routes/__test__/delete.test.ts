978b01eabd452a66d46a6d57d108624466da29a3
panospd <anastapd@gmail.com>
2023-06-04 05:18:58 +0100

    added tests for events

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest"
 import { app } from "../../app"
 import { Ticket } from "../../models/ticket"
 import { Order, OrderStatus } from "../../models/order"
+import { natsWrapper } from "../../nats-wrapper"
 
 it("marks an order as cancelled", async () => {
     const ticket = Ticket.build({
@@ -28,4 +29,35 @@ it("marks an order as cancelled", async () => {
     const updatedOrder = await Order.findById(order.id);
 
     expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
+})
+
+it("marks an order as cancelled", async () => {
+    const ticket = Ticket.build({
+        title: "concert",
+        price: 20
+    })
+
+    await ticket.save()
+
+    const user = signin()
+
+    const { body: order } = await request(app)
+        .post("/api/orders")
+        .set("Cookie", user)
+        .send({ ticketId: ticket.id })
+        .expect(201)
+
+    jest.clearAllMocks()
+
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set("Cookie", user)
+        .send()
+        .expect(204)
+
+    const updatedOrder = await Order.findById(order.id);
+
+    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
+
+    expect(natsWrapper.client.publish).toHaveBeenCalledTimes(1)
 })
