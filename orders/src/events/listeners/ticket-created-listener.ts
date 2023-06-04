3a0b1b89fba3374b965978ee4ed2756a45bde88a
panospd <anastapd@gmail.com>
2023-06-04 06:54:46 +0100

    implemented order service listening on ticket created and updated

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,26 @@
+import { Listener, Subjects, TicketCreatedEvent } from "@ticketingpd/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent>
+{
+    readonly subject = Subjects.TicketCreated;
+    readonly queueGroupName = queueGroupName
+
+    async onMessage(
+        data: TicketCreatedEvent["data"],
+        msg: Message): Promise<void> {
+        const { id, title, price } = data;
+        const ticket = Ticket.build({
+            id,
+            title,
+            price
+        });
+
+        await ticket.save()
+
+        msg.ack()
+    }
+
+}
