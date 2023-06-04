b32427e07501b877f843e78c8f74298fb136a058
panospd <anastapd@gmail.com>
2023-06-04 05:12:42 +0100

    implemented raising event on order creation

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -4,6 +4,8 @@ import { body } from "express-validator";
 import mongoose from "mongoose";
 import { Ticket } from "../models/ticket";
 import { Order } from "../models/order";
+import { OrderCreatedPublisher } from "../events/publishers/order-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -39,6 +41,17 @@ router.post("/api/orders", requireAuth, [
     })
 
     await order.save();
+
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+        id: order.id,
+        status: order.status,
+        expiresAt: order.expiresAt.toISOString(),
+        userId: order.userId,
+        ticket: {
+            id: ticket.id,
+            price: ticket.price
+        }
+    });
 
     res.status(201).send(order)
 })
