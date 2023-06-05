efd109d3d3adb7aa7847132ccd8b7ecde7059c9e
panospd <anastapd@gmail.com>
2023-06-05 06:46:58 +0100

    implemented raising ticket updated event

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -53,3 +53,15 @@ it("acks the message", async () => {
 
     expect(msg.ack).toHaveBeenCalledTimes(1)
 })
+
+it("publishes a ticket updated event", async () => {
+    const { listener, ticket, data, msg } = await setup()
+
+    await listener.onMessage(data, msg)
+
+    expect(natsWrapper.client.publish).toHaveBeenCalledTimes(1);
+
+    const ticketUpdatedData = JSON.parse((natsWrapper.client.publish as jest.Mock).mock.calls[0][1])
+
+    expect(data.id).toEqual(ticketUpdatedData.orderId)
+})
--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@ticketingpd
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>
 {
@@ -17,6 +18,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent>
         ticket.set({ orderId: data.id })
 
         await ticket.save();
+
+        await new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            price: ticket.price,
+            title: ticket.title,
+            userId: ticket.userId,
+            orderId: ticket.orderId,
+            version: ticket.version
+        });
 
         msg.ack()
     }
