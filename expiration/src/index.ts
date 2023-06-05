5cfaf1ec4340d8e5ee9e934b956a8e6b92cfa515
panospd <anastapd@gmail.com>
2023-06-05 17:48:19 +0100

    expiration initial setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,39 @@
+
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+    try {
+
+        if (!process.env.NATS_CLIENT_ID) {
+            throw new Error('NATS_CLIENT_ID must be defined')
+        }
+
+        if (!process.env.NATS_URL) {
+            throw new Error('NATS_URL must be defined')
+        }
+
+        if (!process.env.NATS_CLUSTER_ID) {
+            throw new Error('NATS_CLUSTER_ID must be defined')
+        }
+
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL)
+
+        natsWrapper.client.on("close", () => {
+            console.log("NATS connection closed!")
+            process.exit();
+        })
+
+        process.on("SIGINT", () => natsWrapper.client.close());
+        process.on("SIGTERM", () => natsWrapper.client.close());
+
+        console.log("Good to go!!")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
