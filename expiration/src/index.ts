b6bc90b3df07260be2dcae329b94f7acd7e7bc47
panospd <anastapd@gmail.com>
2023-06-05 18:10:08 +0100

    implemented receiving order created event and queueing job with delay

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
@@ -28,6 +29,8 @@ const start = async () => {
 
         process.on("SIGINT", () => natsWrapper.client.close());
         process.on("SIGTERM", () => natsWrapper.client.close());
+
+        new OrderCreatedListener(natsWrapper.client).listen()
 
         console.log("Good to go!!")
     } catch (error) {
