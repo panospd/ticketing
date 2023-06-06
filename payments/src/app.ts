c22489611c78b74305c1ec390a99aae4372204b9
panospd <anastapd@gmail.com>
2023-06-06 04:57:19 +0100

    payments service - initial setup

---
+++ payments/src/app.ts
@@ -0,0 +1,27 @@
+import "express-async-errors"
+import exprress from "express"
+import "express-async-errors"
+import { json } from "body-parser"
+
+import cookieSession from "cookie-session";
+import { NotFoundError, currentUser, errorHandler } from "@ticketingpd/common";
+
+const app = exprress();
+app.set("trust proxy", true)
+
+app.use(json())
+
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test"
+}))
+
+app.use(currentUser)
+
+app.all("*", async () => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
