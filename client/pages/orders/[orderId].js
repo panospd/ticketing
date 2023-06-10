830263bc3146815532d6dfce4d1dd30f79a4cac9
panospd <anastapd@gmail.com>
2023-06-10 07:38:02 +0100

    implemented re-directing user to orders after ticket purchase

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
 import "prop-types";
 import useRequest from "../../hooks/use-request";
+import Router from "next/router";
 
 const OrderShow = ({ order, currentUser }) => {
     const [timeLeft, setTimeLeft] = useState(0);
@@ -12,7 +13,7 @@ const OrderShow = ({ order, currentUser }) => {
         body: {
             orderId: order.id,
         },
-        onSuccess: (payment) => console.log(payment),
+        onSuccess: () => Router.push("/orders"),
     });
 
     useEffect(() => {
